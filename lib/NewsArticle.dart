5820a3abe415033a3b8921a0028191a4a418cd14
jchoughton <jchoughton@mail.roanoke.edu>
2020-02-28 01:12:19 -0500

    Project part 2 with News Tab with 3 articles

---
+++ lib/NewsArticle.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+final titles = ["Impact Investing is Cool",
+  "Nevis is Amazing",
+  "Help Nevis Help the World"];
+final pictures = ["https://socialcapitalmarkets.net/wp-content/uploads/2019/10/SOCAP19-propel-capital-impact-investing.jpg",
+"https://image.freepik.com/free-photo/wow-face-asian-man-isolated-background_39688-1314.jpg",
+"https://cdn2.hubspot.net/hubfs/2116331/Investing.jpg"];
+final articles = ["Impact Investment is a cool way of investing in ideas across the world. By investing in an idea you can help small companies in that field improve the world.",
+  "Nevis is the best new social app company that will not only let you invest in impact investment but also let you connect with other Nevites like you.",
+  "Nevites like you can support Nevis support companies globaly to improve the world!!!!!"];
+
+class NewsArticle {
+  String title;
+  String picture;
+  String article;
+
+  NewsArticle(int i) {
+    title = titles[i];
+    picture = pictures[i];
+    article = articles[i];
+  }
+
+  String getTitle(int i) {
+    return (title);
+  }
+
+  String getPicture(int i) {
+    return (picture);
+  }
+
+  String getArticle(int i) {
+    return (article);
+  }
+
+  Widget openArticle(int i) {
+    return OpenArticleHelper(i);
+  }
+}
+
+class OpenArticleHelper extends StatelessWidget{
+  NewsArticle pullFrom;
+  OpenArticleHelper(int i) {
+    pullFrom = NewsArticle(i);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(pullFrom.title),
+      ),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          child: Image.network(pullFrom.picture),
+          width: 250,
+          height: 250,
+          ),
+        Flexible(child: Text(pullFrom.article)),
+      ]
+      ),
+    );
+  }
+}
--- lib/NewsTab.dart
+++ lib/NewsTab.dart
@@ -1,16 +1,79 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'NewsArticle.dart';
 
+class NewsTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News',
+      home: NewsTabHelper(),
+    );
+  }
+}
 
-class NewsTab extends StatefulWidget {
+class NewsTabHelper extends StatefulWidget {
   @override
   _NewsTabState createState() {
     return _NewsTabState();
   }
 }
 
-class _NewsTabState extends State<NewsTab> {
+class _NewsTabState extends State<NewsTabHelper> {
+  final _suggestions = <NewsArticle>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _numArticles = 3;
+  // #enddocregion RWS-var
+
+  // #docregion _buildSuggestions
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      itemCount: _numArticles,
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          final index = i%_numArticles;
+          if (index >= _suggestions.length) {
+            NewsArticle temp = NewsArticle(index%_numArticles);
+            _suggestions.add(temp);
+          }
+          return _buildButton(_suggestions[index], index);
+        });
+  }
+  // #enddocregion _buildSuggestions
+
+  // #docregion _buildRow
+  Widget _buildButton(NewsArticle article, int i) {
+    return Row( children: <Widget>[
+      FlatButton(
+        onPressed: (){
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => OpenArticleHelper(i)),
+          );
+        },
+       child: Row(
+           children: <Widget>[
+             Text(
+               article.getTitle(i),
+               style: _biggerFont,
+              ),
+    ])
+      )],
+      );
+    //]);
+  }
+  // #enddocregion _buildRow
+
+  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
-    return Text('NEWS');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('NEWS'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
+// #enddocregion RWS-build
+// #docregion RWS-var
 }
